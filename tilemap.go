b8c37fa0ff75e5bd6b3870d5adce27df0ceece9a
aidan  Coco <aidancoco@aidans-MacBook-Pro.local>
2024-10-07 11:49:11 -0400

    editing tilemap for dynamic tileset

--- tilemap.go
+++ tilemap.go
@@ -3,16 +3,36 @@ package main
 import (
 	"encoding/json"
 	"os"
+	"path"
 )
 
 type TilemapLayerJSON struct {
-	Data   []int `json:"data"`
-	Width  int   `json:"width"`
-	Height int   `json:"height"`
+	Data   []int  `json:"data"`
+	Width  int    `json:"width"`
+	Height int    `json:"height"`
+	Name   string `json:"name"`
 }
 
+// all layers in a tilemap
 type TilemapJSON struct {
 	Layers []TilemapLayerJSON `json:"layers"`
+	// raw data for each tileset
+	Tilesets []map[string]any `json:"tilesets"`
+}
+
+func (t *TilemapJSON) GenTileSets() ([]Tileset, error) {
+
+	tilesets := make([]Tileset, 0)
+
+	for _, tilesetData := range t.Tilesets {
+		tilesetpath := path.Join("assets/map/", tilesetData["source"].(string))
+		tileset, err := NewTileSet(tilesetpath, int(tilesetData["firstgid"].(float64)))
+		if err != nil {
+			return nil, err
+		}
+		tilesets = append(tilesets, tileset)
+	}
+	return tilesets, nil
 }
 
 func NewTilemapJSON(filepath string) (*TilemapJSON, error) {
