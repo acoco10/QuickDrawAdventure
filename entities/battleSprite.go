33594554e62e3f995d9cbd5f3c375f188907f3f1
Aidan Coco <aidancoco@gmail.com>
2024-12-04 15:59:19 -0500

    updating battlesprite funtionality

--- entities/battleSprite.go
+++ entities/battleSprite.go
@@ -3,7 +3,6 @@ package entities
 import (
 	"github.com/acoco10/QuickDrawAdventure/animations"
 	"github.com/acoco10/QuickDrawAdventure/spritesheet"
-
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
@@ -13,8 +12,6 @@ const (
 	Idle SpriteBattleState = iota
 	UsingCombatSkill
 	UsingDialogueSkill
-	TakingDamage
-	BackFacing
 )
 
 type CAnimation uint8
@@ -39,23 +36,69 @@ type BattleSprite struct {
 	SpriteSheet              *spritesheet.SpriteSheet
 	CurrentCombatAnimation   CAnimation
 	CurrentDialogueAnimation DAnimation
-	State                    SpriteBattleState
-	Animations               map[SpriteBattleState]*animations.Animation
-	CombatAnimations         map[CAnimation]*animations.Animation
-	DialogueAnimations       map[DAnimation]*animations.Animation
+	state                    SpriteBattleState
+	IdleAnimation            *animations.Animation
+	CombatAnimations         map[CAnimation]*animations.CyclicAnimation
+	DialogueAnimations       map[DAnimation]*animations.CyclicAnimation
+	counter                  int
+}
+
+func (bs *BattleSprite) UpdateDialogueAnimation(animation DAnimation) {
+	bs.CurrentDialogueAnimation = animation
+}
+
+func (bs *BattleSprite) UpdateCombatAnimation(animation CAnimation) {
+	bs.CurrentCombatAnimation = animation
+}
+
+func (bs *BattleSprite) UpdateState(state SpriteBattleState) {
+	bs.state = state
 }
 
 func (bs *BattleSprite) GetAnimation() *animations.Animation {
 
-	if bs.State == UsingCombatSkill {
-		return bs.CombatAnimations[bs.CurrentCombatAnimation]
+	if bs.state == UsingCombatSkill {
+		return bs.CombatAnimations[bs.CurrentCombatAnimation].Animation
 	}
 
-	if bs.State == UsingDialogueSkill {
-		return bs.DialogueAnimations[bs.CurrentDialogueAnimation]
+	if bs.state == UsingDialogueSkill {
+		return bs.DialogueAnimations[bs.CurrentDialogueAnimation].Animation
+	}
+	if bs.state == Idle {
+		return bs.IdleAnimation
+	}
+	return nil
+}
+
+func (bs *BattleSprite) GetCycles() int {
+	if bs.state == UsingCombatSkill {
+		return bs.CombatAnimations[bs.CurrentCombatAnimation].GetCycles()
+	}
+	if bs.state == UsingDialogueSkill {
+		return bs.DialogueAnimations[bs.CurrentDialogueAnimation].GetCycles()
+	}
+	return 0
+}
+
+func (bs *BattleSprite) Update() {
+	bsAnimation := bs.GetAnimation()
+	if bs.state == UsingCombatSkill || bs.state == UsingDialogueSkill {
+
+		cycles := bs.GetCycles()
+
+		if bsAnimation.Frame() == bsAnimation.LastF {
+			println("counter:", bs.counter, "cycles:", cycles)
+			bs.counter++
+			if bs.counter > cycles {
+				bs.counter = 0
+				bsAnimation.ResetFrame()
+				bs.UpdateState(Idle)
+			}
+		}
 	}
 
-	return bs.Animations[bs.State]
+	bsAnimation.Update()
+
 }
 
 func NewBattleSprite(pImg *ebiten.Image, spriteSheet *spritesheet.SpriteSheet, spawnX float64, spawnY float64) (*BattleSprite, error) {
@@ -67,7 +110,19 @@ func NewBattleSprite(pImg *ebiten.Image, spriteSheet *spritesheet.SpriteSheet, s
 			Y:       spawnY,
 			Visible: true,
 		},
-		Animations: map[SpriteBattleState]*animations.Animation{},
+		CombatAnimations: map[CAnimation]*animations.CyclicAnimation{
+			Shot:        animations.NewCyclicAnimation(5, 12, 7, 7, 1),
+			FocusedShot: animations.NewCyclicAnimation(4, 18, 7, 20, 1),
+			FanShot:     animations.NewCyclicAnimation(3, 17, 7, 10, 3),
+		},
+		DialogueAnimations: map[DAnimation]*animations.CyclicAnimation{
+			Insult:    animations.NewCyclicAnimation(0, 8, 7, 7, 3),
+			Brag:      animations.NewCyclicAnimation(0, 8, 7, 7, 3),
+			StareDown: animations.NewCyclicAnimation(0, 8, 7, 7, 3),
+		},
+		IdleAnimation: animations.NewAnimation(0, 2, 1, 10),
+		counter:       0,
+		state:         Idle,
 	}
 
 	return bSprite, nil
